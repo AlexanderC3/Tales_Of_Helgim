c40bfdcd25a140015fd7b8022ee6099bc49c3850
AlexandeC3U <79148472+AlexandeC3U@users.noreply.github.com>
2023-10-31 23:46:29 +0100

    Added helmet to AnimationTree: sprite auto animates
    When a hat sprite is added it will be able to place it on the player and animates when player idles or moves (depending on sprite sheet)

--- Scripts/CompositeSprites.gd
+++ Scripts/CompositeSprites.gd
@@ -1,10 +1,7 @@
 extends Node
 
 const hat_spritesheet = {
-	0 : preload("res://Assets/Icons/hat_01a.png"),
-	1 : preload("res://Assets/Icons/hat_01b.png"),
-	2 : preload("res://Assets/Icons/hat_01c.png"),
-	3 : preload("res://Assets/Icons/hat_01d.png"),
-	4 : preload("res://Assets/Icons/hat_01e.png"),
-	5 : preload("res://Assets/Icons/hat_01f.png")
+	0 : preload("res://Assets/Icons/hat_1.png"),
+	1 : preload("res://Assets/Icons/hat_2.png"),
+	2 : preload("res://Assets/Icons/hat_3.png")
 }
